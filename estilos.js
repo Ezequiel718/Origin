4c237d1da00e215398d6c0277c9a1455116da065
eze <ese20caballito@gmail.com>
2023-04-21 00:57:58 -0300

    primer commit

---
+++ estilos.js
@@ -0,0 +1,17 @@
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+
+btn1.addEventListener('click', () => {
+  // Acción a realizar cuando se hace clic en el botón 1
+});
+
+btn2.addEventListener('click', () => {
+  // Acción a realizar cuando se hace clic en el botón 2
+});
+
+btn3.addEventListener('click', () => {
+  // Acción a realizar cuando se hace clic en el botón 3
+});
+ 
+// ohohohohohohohoohohohohohohohohohoohohohohohohohohohohohohohohohohoohohohohohohohohohohohohhohoho
